dd2fe11f4117b0d234a6b6f36d7c25f590c125d2
Sam Archie <samuelarchie1@gmail.com>
2021-04-28 10:37:25 +0100

    Adding common folder with foundation enitity

---
+++ MyDocs.Domain/Common/FoundationEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDocs.Domain.Common
+{
+    public class FoundationEntity
+    {
+        public string CreatedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
