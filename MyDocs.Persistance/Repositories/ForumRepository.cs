7a65bdeb3c32d753bffddff5686582e8bd902258
Sam Archie <samuelarchie1@gmail.com>
2021-04-28 13:56:22 +0100

    Adding the concrete repositories for future use along with the services Config which will eventually be implemented in the API Startup

---
+++ MyDocs.Persistance/Repositories/ForumRepository.cs
@@ -0,0 +1,18 @@
+﻿using MyDocs.Application.Contracts.Persistance;
+using MyDocs.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDocs.Persistance.Repositories
+{
+    public class ForumRepository : FoundationRepository<Forum>, IForumRepository
+    {
+        public ForumRepository(MyDocsContext dbContext) : base(dbContext)
+        {
+
+        }
+    }
+}
---
+++ MyDocs.Persistance/Repositories/UserRepository.cs
@@ -0,0 +1,23 @@
+﻿using MyDocs.Application.Contracts.Persistance;
+using MyDocs.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDocs.Persistance.Repositories
+{
+    public class UserRepository : FoundationRepository<User>, IUserRepository
+    {
+        public UserRepository(MyDocsContext dbContext) : base(dbContext)
+        {
+
+        }
+        public Task<bool> IsUserNameAndEmailUnique(string userName, string Email)
+        {
+            var matches = _dbContext.Users.Any(user => user.UserName.Equals(userName) && user.Email.Equals(user.Email));
+            return Task.FromResult(matches);
+        }
+    }
+}
