966cf47f08036c1dc70f118d7a4c02615bd11c05
Sam Archie <samuelarchie1@gmail.com>
2021-04-28 14:39:16 +0100

    serious issues that have been fixed here with regards to empty files

--- MyDocs.Infrastructure/Services/InfrastructureServiceRegistration.cs
+++ MyDocs.Infrastructure/Services/InfrastructureServiceRegistration.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MyDocs.Application.Contracts.Infrastructure;
+using MyDocs.Application.Models.Mail;
+using MyDocs.Infrastructure.Mail;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,15 @@ using System.Threading.Tasks;
 
 namespace MyDocs.Infrastructure.Services
 {
-    class InfrastructureServiceRegistration
+    public static class InfrastructureServiceRegistration
     {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<EmailSettings>(configuration.GetSection("EmailSettings"));
+
+            services.AddTransient<IEmailService, EmailService>();
+
+            return services;
+        }
     }
 }
