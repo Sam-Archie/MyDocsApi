51ec82f58eea625398788ee3962a981a18257c65
Sam Archie <samuelarchie1@gmail.com>
2021-06-05 10:37:27 +0100

    Imroving on init base repo methods to add some more specificity to the post repo, this will b e aprocess that will continue to happen as more complexity is demanded

---
+++ MyDocs.Application/Features/Posts/Queries/GetListOfPostsByUserId/GetListOfPostsByUserQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using MyDocs.Application.Features.Posts.Queries.GetAllPostsQuery;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDocs.Application.Features.Posts.Queries.GetListOfPostsByUserId
+{
+    public class GetListOfPostsByUserQuery : IRequest<PostListVm>
+    {
+        public Guid UserId { get; set; }
+    }
+}
---
+++ MyDocs.Application/Features/Posts/Queries/GetListOfPostsByUserId/GetListOfPostsByUserQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using MyDocs.Application.Features.Posts.Queries.GetAllPostsQuery;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MyDocs.Application.Features.Posts.Queries.GetListOfPostsByUserId
+{
+    public class GetListOfPostsByUserQueryHandler : IRequestHandler<GetListOfPostsByUserQuery, PostListVm>
+    {
+        public GetListOfPostsByUserQueryHandler()
+        {
+
+        }
+
+        public Task<PostListVm> Handle(GetListOfPostsByUserQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
